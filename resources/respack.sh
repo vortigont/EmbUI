1011267c1feb9cb150614c73e6c9db73d2e7c88a
Emil Muratov <gpm@hotplug.ru>
2021-09-01 20:42:05 +0300

    respack - force update option

--- resources/respack.sh
+++ resources/respack.sh
@@ -8,6 +8,29 @@ tags=etags.txt
 refresh_rq=0
 tzcsv=https://raw.githubusercontent.com/nayarsystems/posix_tz_db/master/zones.csv
 
+
+USAGE="Usage: `basename $0` [-h] [-f]"
+
+# parse cmd options
+while getopts hf: OPT; do
+    case "$OPT" in
+        h)
+            echo $USAGE
+            exit 0
+            ;;
+        f)
+            echo "Force refresh"
+            rm -f $tags
+            refresh_rq=1
+            ;;
+        \?)
+            # getopts issues an error message
+            echo $USAGE >&2
+            exit 1
+            ;;
+    esac
+done
+
 [ -f $tags ] || touch $tags
 
 # check github file for a new hash
@@ -36,7 +59,6 @@ cp html/css/*.webp ./data/css/
 cat html/js/*.js | gzip -9 > ./data/js/embui.js.gz
 cat html/index.html | gzip -9 > ./data/index.html.gz
 cat html/favicon.ico | gzip -9 > ./data/favicon.ico.gz
-cp html/.exclude.files ./data/
 
 # update TZ info
 if freshtag ${tzcsv} ; then
@@ -44,7 +66,7 @@ if freshtag ${tzcsv} ; then
     echo '"label","value"' > ./data/zones.csv
     curl -sL $tzcsv >> ./data/zones.csv
     python tzgen.py
-    cat ./data/tz.json | gzip -9 > ./data/js/tz.json.gz
+    gzip -9f ./data/js/tz.json
     rm -f ./data/tz.json ./data/zones.csv
 else
     unzip -o -d ./data/ data.zip "js/tz.json.gz"
@@ -54,7 +76,7 @@ fi
 cd ./data
 zip --filesync -r -0 --quiet ../data.zip ./*
 cd ..
-#rm -r ./data
+rm -r ./data
 
 mv -f newetags.txt $tags
 
