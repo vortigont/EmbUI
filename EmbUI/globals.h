af6b0afd5e81c3001844e4ac97cd56265b83c415
Emil Muratov <gpm@hotplug.ru>
2024-06-21 20:48:44 +0900

    bump pkg version to v3.2.1

--- EmbUI/globals.h
+++ EmbUI/globals.h
@@ -23,7 +23,7 @@
 
 #define EMBUI_VERSION_MAJOR     3
 #define EMBUI_VERSION_MINOR     2
-#define EMBUI_VERSION_REVISION  0
+#define EMBUI_VERSION_REVISION  1
 
 // API version for JS frontend
 #define EMBUI_JSAPI             4
