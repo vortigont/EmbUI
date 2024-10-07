f71c79728f5a4e54318af7b8bf3218a3ba8e6162
Emil Muratov <gpm@hotplug.ru>
2024-10-07 23:18:33 +0900

    bump ui_sys objects version

--- EmbUI/globals.h
+++ EmbUI/globals.h
@@ -28,7 +28,7 @@
 // API version for JS frontend
 #define EMBUI_JSAPI             4
 // loadable UI blocks version requirement (loaded from js/ui_sys.json)
-#define EMBUI_UIOBJECTS         1
+#define EMBUI_UIOBJECTS         3
 
 #define EMBUI_VERSION_VALUE     (MAJ, MIN, REV) ((MAJ) << 16 | (MIN) << 8 | (REV))
 
