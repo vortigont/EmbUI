275b6933148f0afa4ebde7e8afb320e923b4888c
Emil Muratov <gpm@hotplug.ru>
2025-05-23 10:31:12 +0900

    Rel 4.2.2
     - ActionHandler accepts function arg as reference
     - fix: refactor NTPoDHCP
     - embuifs - make deserializeFile as templates
     - small time config related changes
     - fix AP password converted to 'null' if unconfigured
     - update lib deps

--- src/embui_defines.h
+++ src/embui_defines.h
@@ -15,7 +15,7 @@
 
 #define EMBUI_VERSION_MAJOR     4
 #define EMBUI_VERSION_MINOR     2
-#define EMBUI_VERSION_REVISION  1
+#define EMBUI_VERSION_REVISION  2
 
 // API version for JS frontend
 #define EMBUI_JSAPI             8
